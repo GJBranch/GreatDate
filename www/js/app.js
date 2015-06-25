2a50c1968ba64df8acfc966332cf8221a61a80ff
Gerry Branch <gerry.branch@afrmic.com>
2015-06-24 23:16:41 -0500

    Switch app from sidemenu to tabs

--- www/js/app.js
+++ www/js/app.js
@@ -1,72 +1,83 @@
-// Ionic Starter App
+// Ionic GreatDate App
 
 // angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// 'GreatDate' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-// 'starter.controllers' is found in controllers.js
-angular.module('GreatDate', ['ionic', 'GreatDate.controllers'])
+// 'GreatDate.services' is found in services.js
+// 'GreatDate.controllers' is found in controllers.js
+angular.module('GreatDate', ['ionic', 'GreatDate.controllers', 'GreatDate.services'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
-
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
+    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
     }
     if (window.StatusBar) {
       // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
+      StatusBar.styleLightContent();
     }
   });
 })
 
 .config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
   $stateProvider
 
-  .state('greatDate', {
-    url: "/app",
+  // setup an abstract state for the tabs directive
+    .state('tab', {
+    url: "/tab",
     abstract: true,
-    templateUrl: "templates/menu.html",
-    controller: 'GreatDateController'
+    templateUrl: "templates/tabs.html"
   })
 
-  .state('greatDate.search', {
-    url: "/search",
+  // Each tab has its own nav history stack:
+
+  .state('tab.dash', {
+    url: '/dash',
     views: {
-      'menuContent': {
-        templateUrl: "templates/search.html"
+      'tab-dash': {
+        templateUrl: 'templates/tab-dash.html',
+        controller: 'DashCtrl'
       }
     }
   })
 
-  .state('greatDate.browse', {
-    url: "/browse",
-    views: {
-      'menuContent': {
-        templateUrl: "templates/browse.html"
-      }
-    }
-  })
-    .state('greatDate.recommendations', {
-      url: "/recommendations",
+  .state('tab.chats', {
+      url: '/chats',
       views: {
-        'menuContent': {
-          templateUrl: "templates/recommendations.html",
-          controller: 'RecommendationsController'
+        'tab-chats': {
+          templateUrl: 'templates/tab-chats.html',
+          controller: 'ChatsCtrl'
+        }
+      }
+    })
+    .state('tab.chat-detail', {
+      url: '/chats/:chatId',
+      views: {
+        'tab-chats': {
+          templateUrl: 'templates/chat-detail.html',
+          controller: 'ChatDetailCtrl'
         }
       }
     })
 
-  .state('greatDate.single', {
-    url: "/recommendations/:recommendationId",
+  .state('tab.account', {
+    url: '/account',
     views: {
-      'menuContent': {
-        templateUrl: "templates/recommendation.html",
-        controller: 'RecommendationController'
+      'tab-account': {
+        templateUrl: 'templates/tab-account.html',
+        controller: 'AccountCtrl'
       }
     }
   });
+
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/recommendations');
+  $urlRouterProvider.otherwise('/tab/dash');
+
 });
